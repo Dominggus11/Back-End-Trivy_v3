cb4c34c7279f63cbb395d54e9be801270c38a667
Dominggus11 <roymalau12304@gmail.com>
2022-09-14 17:12:44 +0700

    tks God

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"trivy_v3/controllers"
 	"trivy_v3/models"
 
 	"github.com/gin-gonic/gin"
@@ -9,5 +10,7 @@ import (
 func main() {
 	r := gin.Default()
 	models.ConnectDatabase() // new
+	// router.Router()
+	r.GET("/getAll", controllers.FindAll)
 	r.Run()
 }
--- router/route.go
+++ router/route.go
@@ -1,1 +1,14 @@
 package router
+
+import (
+	"trivy_v3/controllers"
+
+	"github.com/gin-gonic/gin"
+)
+
+func Router() {
+	r := gin.Default()
+
+	r.GET("/getAll", controllers.FindAll)
+	r.POST("/upload", controllers.PostDockerfile)
+}
