c98151ff9c3b083640392ab533d73e8bb7cd1482
Dominggus11 <roymalau12304@gmail.com>
2022-09-14 20:14:38 +0700

    Thanks God

--- main.go
+++ main.go
@@ -6,5 +6,5 @@ import (
 
 func main() {
 	// router.Router()
-	trivy.MkdirDocker()
+	trivy.MkdirWrite()
 }
--- trivy/Mkdir.go
+++ trivy/Mkdir.go
@@ -1,22 +1,37 @@
 package trivy
 
 import (
-	"fmt"
 	"os"
 	"os/exec"
 	"strconv"
 )
 
-func MkdirDocker() {
+func MkdirUpload() {
 	folder := 1
-	pathFolder := "FileDocker/"
+	pathFolder := "FileDocker/FileUpload/"
 	for i := 1; i < 10; i++ {
 		temp := strconv.Itoa(folder)
 		_, err := os.Stat(pathFolder + temp)
 		if err == nil {
 			folder = folder + 1
 		} else {
-			fmt.Println("Tidak Ada")
+			cmdUpload := exec.Command("mkdir", pathFolder+temp)
+			cmdUpload.Stdout = os.Stdout
+			cmdUpload.Run()
+			break
+		}
+	}
+}
+
+func MkdirWrite() {
+	folder := 1
+	pathFolder := "FileDocker/FileWrite/"
+	for i := 1; i < 10; i++ {
+		temp := strconv.Itoa(folder)
+		_, err := os.Stat(pathFolder + temp)
+		if err == nil {
+			folder = folder + 1
+		} else {
 			cmdUpload := exec.Command("mkdir", pathFolder+temp)
 			cmdUpload.Stdout = os.Stdout
 			cmdUpload.Run()
