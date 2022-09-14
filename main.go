e709e7fef863515cd1023512c6347c705701d499
Dominggus11 <roymalau12304@gmail.com>
2022-09-14 20:31:03 +0700

    Thanks God

--- main.go
+++ main.go
@@ -1,10 +1,11 @@
 package main
 
-import (
-	"trivy_v3/trivy"
-)
+import "trivy_v3/trivy"
 
 func main() {
 	// router.Router()
-	trivy.MkdirWrite()
+	trivy.MkdirUploadFile()
+	trivy.MkdirUploadJson()
+	trivy.MkdirWriteFile()
+	trivy.MkdirWriteJson()
 }
--- trivy/Mkdir.go
+++ trivy/Mkdir.go
@@ -1,21 +1,22 @@
 package trivy
 
 import (
+	"fmt"
 	"os"
 	"os/exec"
 	"strconv"
 )
 
-func MkdirUpload() {
+func MkdirUploadFile() {
 	folder := 1
 	pathFolder := "FileDocker/FileUpload/"
 	for i := 1; i < 10; i++ {
-		temp := strconv.Itoa(folder)
-		_, err := os.Stat(pathFolder + temp)
+		_, err := os.Stat(pathFolder + strconv.Itoa(folder))
 		if err == nil {
 			folder = folder + 1
 		} else {
-			cmdUpload := exec.Command("mkdir", pathFolder+temp)
+			fmt.Println(pathFolder + strconv.Itoa(folder))
+			cmdUpload := exec.Command("mkdir", pathFolder+strconv.Itoa(folder))
 			cmdUpload.Stdout = os.Stdout
 			cmdUpload.Run()
 			break
@@ -23,16 +24,47 @@ func MkdirUpload() {
 	}
 }
 
-func MkdirWrite() {
+func MkdirWriteFile() {
 	folder := 1
 	pathFolder := "FileDocker/FileWrite/"
 	for i := 1; i < 10; i++ {
-		temp := strconv.Itoa(folder)
-		_, err := os.Stat(pathFolder + temp)
+		_, err := os.Stat(pathFolder + strconv.Itoa(folder))
 		if err == nil {
 			folder = folder + 1
 		} else {
-			cmdUpload := exec.Command("mkdir", pathFolder+temp)
+			cmdUpload := exec.Command("mkdir", pathFolder+strconv.Itoa(folder))
+			cmdUpload.Stdout = os.Stdout
+			cmdUpload.Run()
+			break
+		}
+	}
+}
+
+func MkdirWriteJson() {
+	folder := 1
+	pathFolder := "FileJson/FileWrite/"
+	for i := 1; i < 10; i++ {
+		_, err := os.Stat(pathFolder + strconv.Itoa(folder))
+		if err == nil {
+			folder = folder + 1
+		} else {
+			cmdUpload := exec.Command("mkdir", pathFolder+strconv.Itoa(folder))
+			cmdUpload.Stdout = os.Stdout
+			cmdUpload.Run()
+			break
+		}
+	}
+}
+
+func MkdirUploadJson() {
+	folder := 1
+	pathFolder := "FileJson/FileUpload/"
+	for i := 1; i < 10; i++ {
+		_, err := os.Stat(pathFolder + strconv.Itoa(folder))
+		if err == nil {
+			folder = folder + 1
+		} else {
+			cmdUpload := exec.Command("mkdir", pathFolder+strconv.Itoa(folder))
 			cmdUpload.Stdout = os.Stdout
 			cmdUpload.Run()
 			break
