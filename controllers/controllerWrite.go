f3d045cb80359d461cdc71648f09be52ccfd960e
Dominggus11 <roymalau12304@gmail.com>
2022-09-15 09:40:13 +0700

    Happy Dukdak

---
+++ controllers/controllerWrite.go
@@ -0,0 +1,23 @@
+package controllers
+
+import "github.com/gin-gonic/gin"
+
+func FindCodes(c *gin.Context) {
+
+}
+
+func FindCode(c *gin.Context) {
+
+}
+
+func PostCode(c *gin.Context) {
+
+}
+
+func UpdateCode(c *gin.Context) {
+
+}
+
+func DeleteCode(c *gin.Context) {
+
+}
--- router/route.go
+++ router/route.go
@@ -10,12 +10,6 @@ import (
 func Router() {
 	r := gin.Default()
 	models.ConnectDatabase()
-	// untuk API Upload
-	r.GET("/getUploads", controllers.FindDockers)
-	r.POST("/postUpload", controllers.PostDockerfile)
-	r.GET("/getUpload/:id", controllers.FindDocker)
-	r.PUT("/updateUpload/:id", controllers.UpdateDocker)
-	r.DELETE("/deleteUpload/:id", controllers.DeleteDocker)
 
 	// untuk API Project
 	r.GET("/getProkjects", controllers.FindProjects)
@@ -23,8 +17,23 @@ func Router() {
 	r.POST("/postProject", controllers.PostProject)
 	r.PUT("/updateProject", controllers.UpdateProject)
 	r.DELETE("/deleteProject", controllers.DeleteProject)
-	r.Run()
+
+	// untuk API Upload
+	r.GET("/getUploads", controllers.FindDockers)
+	r.POST("/postUpload", controllers.PostDockerfile)
+	r.GET("/getUpload/:id", controllers.FindDocker)
+	r.PUT("/updateUpload/:id", controllers.UpdateDocker)
+	r.DELETE("/deleteUpload/:id", controllers.DeleteDocker)
+
+	// untuk API Code
+	r.GET("/getCodes", controllers.FindCodes)
+	r.POST("/postCode", controllers.PostCode)
+	r.GET("/getCode/:id", controllers.FindCode)
+	r.PUT("/updateCode/:id", controllers.UpdateCode)
+	r.DELETE("/deleteCode/:id", controllers.DeleteCode)
 
 	// untuk API return JSON
 	r.GET("/jsonfile/:id", controllers.GetJson)
+
+	r.Run()
 }
